39caf5b903b5f44402334d6c9b7bae4d92d029e1
kangik <kangik@neuroimage.snu.ac.kr>
2015-06-30 13:54:54 +0900

    working version

--- CSD_extraction.py
+++ CSD_extraction.py
@@ -13,20 +13,27 @@ pd.set_option('display.width', 1000)
 
 def main(args):
 
-    # if args.input present
+    # If args.input present
     if args.input:
         current_file = args.input +'_current.csv'
         dipole_file = args.input +'_dipole.csv'
-        source_file = args.input +'_source.csv'
+        source_file = args.input +'_source.rtf'
     else:
         current_file = args.current
         dipole_file = args.dipole
         source_file = args.source
 
-    # dipole table location to strength time series
+    # Get information from the source file
+    CDR_results = get_info_from_source(source_file)
 
-    region_table = find_location(dipole_file)
+    # returns dict eg) {'x': 13,'y':-2,'z':60}
+    source_CDR_table = get_coord_from_source(CDR_results) 
+
+    # Read files + get location + add source_CDR location
+    region_table = find_location(dipole_file,source_CDR_table)
+    print region_table
     currentDf = pd.read_csv(current_file)
+
     strength_timeseries_list = coord_to_strength_time(currentDf,region_table)
     finalDf = pd.concat(strength_timeseries_list,axis=1)
 
@@ -38,35 +45,6 @@ def main(args):
         else:
             finalDf.to_csv(args.output+'_out.csv')
 
-    
-    # source table CDR location to strength time series
-    CDR_results, SNR, CDR_dipole_results = get_info_from_source(source_file)
-    source_CDR_table = get_coord_from_source(CDR_results)
-    strength_timeseries_CDR_source = coord_to_strength_time(currentDf,
-                                                            source_CDR_table)
-    finalDf_CDR_source = pd.concat(strength_timeseries_CDR_source, axis=1)
-
-    if not args.output:
-        finalDf_CDR_source.to_csv(current_file.split('.')[0]+'_source_CDR_out.csv')
-    else:
-        if args.output.endswith('.csv'):
-            finalDf_CDR_source.to_csv('_source_CDR_'+args.output)
-        else:
-            finalDf_CDR_source.to_csv('_source_CDR_'+args.output+'_out.csv')
-
-
-    source_dipole_table = get_coord_from_source(CDR_dipole_results)
-    strength_timeseries_dipole_source = coord_to_strength_time(currentDf,
-                                                            source_dipole_table)
-    finalDf_dipole_source = pd.concat(strength_timeseries_dipole_source, axis=1)
-
-    if not args.output:
-        finalDf_dipole_source.to_csv(current_file.split('.')[0]+'_source_dipole_out.csv')
-    else:
-        if args.output.endswith('.csv'):
-            finalDf_dipole_source.to_csv('_source_dipole_'+args.output)
-        else:
-            finalDf_dipole_source.to_csv('_source_dipole_'+args.output+'_out.csv')
 
 def coord_to_strength_time(currentDf, coordsList):
     '''
@@ -103,7 +81,7 @@ def coord_to_strength_time(currentDf, coordsList):
     return strength_timeseries_list
 
 
-def find_location(dipoleCSV):
+def find_location(dipoleCSV,source_CDR_table):
     dipoleDf = pd.read_csv(dipoleCSV)
     region_table = {}
     for region in dipoleDf[' atlas'].unique():
@@ -117,35 +95,29 @@ def find_location(dipoleCSV):
             
         region_table[region] = coord_list
     
+    region_table['source CDR'] = [source_CDR_table]
     return region_table
 
 
 def get_info_from_source(sourceCSV):
     with open(sourceCSV) as f:
         source_lines = f.read().split('\\\n\\\n')
-
-        print source_lines
-        
         for i in source_lines:
             if i.startswith('CDR Results'):
                 CDR_results = i
+                #print CDR_results
                 CDR_results_sources = re.search('(\d+) sources',i).group(1)
 
-            elif i.startswith('\tSNR'):
-                SNR = i
-                SNR = SNR.split('\t')[1]
-            elif i.startswith('CDR Dipole'):
-                CDR_dipole_results = i
-
-    return CDR_results_sources, SNR, CDR_dipole_results
+    return CDR_results
 
 def get_coord_from_source(source):
-    coord = re.search(': \((.*)\)mm',CDR_results).group(1).split(', ')
+    #print source,'source'
+    coord = re.search(': \((.*)\)mm',source).group(1).split(', ')
     coord_float = [float(x) for x in coord]
     coord_dict = {'x':coord_float[0], 
                   'y':coord_float[1],
                   'z':coord_float[2]}
-    return [coord_dict]
+    return coord_dict
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -184,9 +156,9 @@ if __name__ == '__main__':
     if args.input:
         if not args.dipole or not args.current:
             print '\tThis will automatically grep'
-            print '\t\t{0}_current.csv'.format(args.input+'_current.csv')
-            print '\t\t{0}_current.csv'.format(args.input+'_dipole.csv')
-            print '\t\t{0}_current.csv'.format(args.input+'_source.csv')
+            print '\t\t{0}'.format(args.input+'_current.csv')
+            print '\t\t{0}'.format(args.input+'_dipole.csv')
+            print '\t\t{0}'.format(args.input+'_source.rtf')
             
     else:
         if not args.dipole or not args.current:
